54a777302bba20ca9b64fa0996bb395fd3866480
lxp <1432355905@qq.com>
2021-05-27 14:57:41 +0800

    First Commit

---
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -0,0 +1,15 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	while (true)
+	{
+		cout << 1 << endl;
+	}
+	
+	return 0;
+}
